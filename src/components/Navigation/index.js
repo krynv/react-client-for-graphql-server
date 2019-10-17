077190bc6d0b2bb969b1f0b3714173210e3a2599
VK <vitaliy.krynytskyy@gmail.com>
2019-10-17 16:22:38 +0100

    Added Sign In

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,24 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import * as routes from '../../constants/routes';
+import SignOutButton from '../SignOut';
 
-const Navigation = () => (
+const Navigation = ({ session }) => (
     <div>
-        <ul>
-            <li>
-                <Link to={routes.SIGN_IN}>Sign In</Link>
-            </li>
-            <li>
-                <Link to={routes.SIGN_UP}>Sign Up</Link>
-            </li>
-            <li>
-                <Link to={routes.HOME}>Home</Link>
-            </li>
-            <li>
-                <Link to={routes.ACCOUNT}>Account</Link>
-            </li>
-        </ul>
+        {session && session.me ? (
+            <NavigationAuth session={session} />
+        ) : (
+                <NavigationNonAuth />
+            )}
     </div>
+);
+
+const NavigationAuth = ({ session }) => (
+    <ul>
+        <li>
+            <Link to={routes.HOME}>Home</Link>
+        </li>
+        <li>
+            <Link to={routes.ACCOUNT}>Account ({session.me.username})</Link>
+        </li>
+        <li>
+            <SignOutButton />
+        </li>
+    </ul>
+);
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+            <Link to={routes.SIGN_IN}>Sign In</Link>
+        </li>
+        <li>
+            <Link to={routes.HOME}>Home</Link>
+        </li>
+    </ul>
 );
 
 export default Navigation;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -21,11 +21,11 @@ const INITIAL_STATE = {
     confirmPassword: '',
 };
 
-const SignUpPage = ({ history }) => (
+const SignUpPage = ({ history, refetch }) => (
     <div>
         <h1>Sign Up</h1>
 
-        <SignUpForm history={history} />
+        <SignUpForm history={history} refetch={refetch} />
     </div>
 );
 
@@ -39,8 +39,12 @@ class SignUpForm extends React.Component {
     };
 
     onSubmit = (event, signUp) => {
-        signUp().then(data => {
+        signUp().then(async ({ data }) => {
             this.setState({ ...INITIAL_STATE });
+
+            localStorage.setItem('token', data.signUp.token);
+
+            await this.props.refetch;
 
             this.props.history.push(routes.HOME);
         });
