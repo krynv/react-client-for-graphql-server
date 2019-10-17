d004eeb491fbb147f1e5d4d437b14fbe99777534
VK <vitaliy.krynytskyy@gmail.com>
2019-10-17 13:55:16 +0100

    Reads data from the back end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>test</p>
-    </div>
-  );
+const GET_USERS_WITH_MESSAGES = gql`
+{
+    users {
+        id
+        username
+        email
+        messages {
+            id
+            text
+            createdAt
+        }
+    }
 }
+`;
+
+const App = () => (
+  <Query query={GET_USERS_WITH_MESSAGES}>
+    {({ loading, error, data }) => {
+
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <div>{data.users.map(user => (
+          <div key={user.id}>
+            <h2>{user.username}</h2>
+            <div>
+              {user.messages.map(message => (
+                <div key={message.id}>{message.text}</div>
+              ))}
+            </div>
+          </div>
+        ))}</div>
+      );
+    }}
+  </Query>
+);
 
 export default App;
